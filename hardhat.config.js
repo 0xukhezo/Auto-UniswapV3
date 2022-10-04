36f5878b0c51e5d6ab2517dcb2f8a5c82c62e5d8
0xukhezo <ukhezo.web3@gmail.com>
2022-10-04 09:36:38 +0200

    Front end set up

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("hardhat-deploy");
+require("solidity-coverage");
+
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.7.6",
+      },
+    ],
+  },
+};
